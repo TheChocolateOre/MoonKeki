2a7423e5e23ac8dca50155578dc4ce9e70a556de
TheChocolateOre <50585073+TheChocolateOre@users.noreply.github.com>
2021-01-09 19:43:04 +0200

    Added Renderer.java work in progress

---
+++ src/main/java/render/Renderer.java
@@ -0,0 +1,154 @@
+package render;
+
+import org.lwjgl.system.MemoryUtil;
+
+import java.awt.geom.AffineTransform;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Renderer implements AutoCloseable {
+
+    private static final int VERTICES_PER_QUAD = 6;
+    private static final int FLOATS_PER_QUAD = 4 * VERTICES_PER_QUAD;
+    //contains texture coordinates too
+    private FloatBuffer vertices;
+    private int size;
+    private ShaderProgram baseProgram = ShaderProgram.DEFAULT;
+    private final List<ShaderProgram> POST_PROGRAMS = new ArrayList<>();
+    @Deprecated
+    private ShaderProgram activeProgram;
+    private Pixmap destination; //null to render on screen
+    private final AffineTransform TRANSFORM = new AffineTransform();
+    private boolean closed;
+
+    public Renderer() {
+        //96kB
+        this(1000);
+    }
+
+    //Capacity is the maximum number of draw() calls before flush(). A draw()
+    //call has 24 vertices
+    public Renderer(int capacity) {
+        this.vertices = MemoryUtil.memAllocFloat(Math.multiplyExact(
+                FLOATS_PER_QUAD, capacity));
+    }
+
+    public void draw(Pixmap pixmap) {
+        this.draw(pixmap, 0.0f, 0.0f);
+    }
+
+    public void draw(Pixmap pixmap, float x, float y) {
+        this.draw(pixmap, 0.0f, 0.0f, pixmap.getWidth(),
+                pixmap.getHeight());
+    }
+
+    public void draw(Pixmap pixmap, float x, float y, float width, float
+            height) {
+        throw new UnsupportedOperationException();
+    }
+
+    //null to render on screen
+    public void setDestination(Pixmap pixmap) {
+        this.ensureOpen();
+        throw new UnsupportedOperationException();
+    }
+
+    public ShaderProgram getBaseProgram() {
+        this.ensureOpen();
+        return this.baseProgram;
+    }
+
+    public void setBaseProgram(ShaderProgram program) {
+        this.ensureOpen();
+        this.baseProgram = program;
+    }
+
+    public List<ShaderProgram> getPostPrograms() {
+        this.ensureOpen();
+        return Collections.unmodifiableList(this.POST_PROGRAMS);
+    }
+
+    public void setPostPrograms(List<ShaderProgram> programs) {
+        this.ensureOpen();
+
+        if (programs.stream().anyMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("Argument List programs can't " +
+                    "contain null values.");
+        }//end if
+
+        this.POST_PROGRAMS.clear();
+        this.POST_PROGRAMS.addAll(programs);
+    }
+
+    public void setTransform(AffineTransform transform) {
+        this.ensureOpen();
+        this.TRANSFORM.setTransform(transform);
+    }
+
+    public void flush() {
+        this.ensureOpen();
+
+        if (this.isEmpty()) {
+            return;
+        }//end if
+
+        //Stuff..
+
+        this.clear();
+        throw new UnsupportedOperationException();
+    }
+
+    public int size() {
+        this.ensureOpen();
+        return this.size;
+    }
+
+    public int capacity() {
+        this.ensureOpen();
+        return this.vertices.capacity() / FLOATS_PER_QUAD;
+    }
+
+    public int space() {
+        return this.capacity() - this.size();
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    public void clear() {
+        this.ensureOpen();
+        this.vertices.clear();
+        this.size = 0;
+    }
+
+    public boolean isClosed() {
+        return this.closed;
+    }
+
+    @Override
+    public void close() {
+        if (this.isClosed()) {
+            return;
+        }//end if
+
+        MemoryUtil.memFree(this.vertices);
+        this.closed = true;
+    }
+
+    @Deprecated
+    private void useProgram(ShaderProgram program) {
+        program.use();
+        this.activeProgram = program;
+    }
+
+    private void ensureOpen() throws IllegalStateException {
+        if (this.isClosed()) {
+            throw new IllegalStateException("This Renderer is closed.");
+        }//end if
+    }
+
+}//end class Renderer
