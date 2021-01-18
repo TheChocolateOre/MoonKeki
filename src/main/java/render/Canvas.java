5eca4f46e8fcab55f8dbf0d70e0432606677a0e6
TheChocolateOre <50585073+TheChocolateOre@users.noreply.github.com>
2021-01-18 20:13:13 +0200

    Added Canvas.java

---
+++ src/main/java/render/Canvas.java
@@ -0,0 +1,34 @@
+package render;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+
+abstract class Canvas {
+
+    static record Bounds(int x, int y, int width, int height) {
+        Bounds {
+            if (width < 0) {
+                throw new IllegalArgumentException("Argument width can't be " +
+                        "negative.");
+            }//end if
+
+            if (height < 0) {
+                throw new IllegalArgumentException("Argument height can't be " +
+                        "negative.");
+            }//end if
+        }
+    }//end static nested record Bounds
+
+    void setup(int framebufferId) {
+        Bounds bounds = this.getBounds();
+        GL11.glViewport(bounds.x, bounds.y, bounds.width, bounds.height);
+        GL11.glEnable(GL11.GL_SCISSOR_TEST);
+        GL11.glScissor(bounds.x, bounds.y, bounds.width, bounds.height);
+        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, framebufferId);
+    }
+
+    abstract Canvas.Bounds getBounds();
+    abstract boolean isVoid();
+    abstract int getId();
+
+}//end class Canvas
