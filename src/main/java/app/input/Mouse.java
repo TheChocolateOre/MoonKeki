391e59ddfe86974bee9c9c8742c3fb7754059ccc
TheChocolateOre <50585073+TheChocolateOre@users.noreply.github.com>
2021-06-08 09:41:49 +0300

    Added support for mouse position events

--- src/main/java/app/input/Mouse.java
+++ src/main/java/app/input/Mouse.java
@@ -52,6 +52,18 @@ public final class Mouse {
             });
         }
     }//end static nested class Button
+    
+    private static class PositionEvent extends Event {
+    
+        final BiPredicate<Double, Double> HOVER_PREDICATE;
+        
+        PositionEvent(boolean isHappening, BiPredicate<Double, Double> 
+                      hoverPredicate) {
+            super(isHappening);
+            this.HOVER_PREDICATE = hoverPredicate;
+        }
+        
+    }//end static nested class PositionEvent
 
     //id -> button (only for non-named buttons, if exist)
     private static final Map<Integer, Mouse.Button> ID_TO_BUTTON =
@@ -61,10 +73,14 @@ public final class Mouse {
             new ReentrantReadWriteLock(true);
     private static final ReadWriteLock RELEASED_EVENTS_LOCK =
             new ReentrantReadWriteLock(true);
+    private static final ReadWriteLock POSITION_EVENTS_LOCK =
+            new ReentrantReadWriteLock(true);
     private static final Map<Mouse.Button, Set<Event>> PRESSED_EVENTS =
             new HashMap<>();
     private static final Map<Mouse.Button, Set<Event>> RELEASED_EVENTS =
             new HashMap<>();
+    private static final Collection<PositionEvent> POSITION_EVENTS =
+            new LinkedHashSet<>();
 
     private static final Queue<ButtonSeeker> pressedButtonSeekers =
             new ConcurrentLinkedQueue<>();
@@ -95,16 +111,6 @@ public final class Mouse {
             if (buttonSeeker != null) {
                 buttonSeeker.putIfAbsent(BUTTON);
             }//end if
-
-            //We process the ButtonEvent's'
-            var buttonSnapshot = new Mouse.Button.Snapshot(BUTTON, PRESSED);
-            Mouse.BUTTON_EVENTS_LOCK.readLock().lock();
-            try {
-                Mouse.BUTTON_EVENTS.getOrDefault(BUTTON, Collections.emptySet())
-                                   .forEach(e -> e.update(buttonSnapshot));
-            } finally {
-                Mouse.BUTTON_EVENTS_LOCK.readLock().unlock();
-            }//end try
             
             //We process the Event's'
             final ReadWriteLock LOCK;
@@ -139,6 +145,22 @@ public final class Mouse {
                 OTHER_EVENTS.forEach(Event::stop);
             } finally {
                 OTHER_LOCK.readLock().unlock();
+            }//end try
+        });
+        
+        GLFW.glfwSetCursorPosCallback(GLFW.glfwGetCurrentContext(), (long 
+                window, double x, double y) -> {
+            POSITION_EVENTS_LOCK.readLock().lock();
+            try {
+                Mouse.POSITION_EVENTS.forEach(e -> {
+                    if (e.POSITION_PREDICATE.test(x, y)) {
+                        e.start();
+                    } else {
+                        e.stop();
+                    }//end if
+                });
+            } finally {
+                POSITION_EVENTS_LOCK.readLock().unlock();
             }//end try
         });
     }//end static initializer
@@ -178,6 +200,32 @@ public final class Mouse {
 
         return EVENT.asConsumer();
     }
+    
+    public static Event.Consumer positionEvent(BiPredicate<Double, Double> 
+                                               hoverPredicate) {
+        final Event POSITION_EVENT = new PositionEvent(button.isPressed() == pressed,
+                                                       hoverPredicate) {
+            @Override
+            public void close() {
+                super.close();
+                Mouse.POSITION_EVENTS_LOCK.writeLock().lock();
+                try {
+                    Mouse.POSITION_EVENTS.remove(this);
+                } finally {
+                    Mouse.POSITION_EVENTS_LOCK.writeLock().unlock();
+                }//end try
+            }
+        };
+
+        Mouse.POSITION_EVENTS_LOCK.writeLock().lock();
+        try {
+            Mouse.POSITION_EVENTS.add(POSITION_EVENT);
+        } finally {
+            Mouse.POSITION_EVENTS_LOCK.writeLock().unlock();
+        }//end try
+
+        return EVENT.asConsumer();
+    }
 
     public static ButtonSeeker seekNextButton(final boolean pressed) {
         ButtonSeeker buttonSeeker = new ButtonSeeker();
@@ -188,26 +236,6 @@ public final class Mouse {
         }//end if
 
         return buttonSeeker;
-    }
-
-    static void addButtonEvent(Mouse.Button button, ButtonEventOld event) {
-        Mouse.BUTTON_EVENTS_LOCK.writeLock().lock();
-        try {
-            Mouse.BUTTON_EVENTS.computeIfAbsent(button, k ->
-                    new LinkedHashSet<>()).add(event);
-        } finally {
-            Mouse.BUTTON_EVENTS_LOCK.writeLock().unlock();
-        }//end try
-    }
-
-    static void removeButtonEvent(Mouse.Button button, ButtonEventOld event) {
-        Mouse.BUTTON_EVENTS_LOCK.writeLock().lock();
-        try {
-            Mouse.BUTTON_EVENTS.getOrDefault(button, Collections.emptySet())
-                    .remove(event);
-        } finally {
-            Mouse.BUTTON_EVENTS_LOCK.writeLock().unlock();
-        }//end try
     }
 
     //trust-based, no argument validation
